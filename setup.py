4d252e462773176d54dbebfb2d87774675f8a396
dougiesquire <dougiesquire@gmail.com>
2021-11-23 10:09:18 +1100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Skill benchmarking of the CAFE-f6 decadal forecasts',
+    author='Dougie Squire',
+    license='MIT',
+)
