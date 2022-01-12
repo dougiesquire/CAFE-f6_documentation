b54aad02caf4063a279ef63fb9b8ee3a6292aaf8
dougiesquire <dougiesquire@gmail.com>
2022-01-13 08:15:11 +1100

    open_mfzarr in parallel

--- src/utils/io.py
+++ src/utils/io.py
@@ -4,9 +4,8 @@ import xarray as xr
 def open_mfzarr(files, preprocess=None, parallel=False, **open_kwargs):
     """ Open multiple zarr files applying a preprocess step prior to merging 
         
-        NOTE: for some reason, using parallel=True produces cftime.datetime objects
-        that break with DatatimeAccessor methods like dt.floor. Opening the data in 
-        serial does not, see https://github.com/pydata/xarray/issues/6026
+        NOTE: with cftime < 1.5.1, using parallel=True produces cftime.datetime objects
+        that break with DatatimeAccessor methods like dt.floor.
     """
     open_zarr_ = dask.delayed(xr.open_zarr) if parallel else xr.open_zarr
     preprocess_ = dask.delayed(preprocess) if parallel else preprocess
