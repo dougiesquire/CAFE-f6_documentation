bb0823697f5b7908a1dc2c34f8db21e0b206df54
dougiesquire <dougiesquire@gmail.com>
2022-01-27 11:13:50 +1100

    test forecast reproducibility

--- notebooks/forecast_issues/what_is_different_between_MOM_summarys.sh
+++ notebooks/forecast_issues/what_is_different_between_MOM_summarys.sh
@@ -3,8 +3,11 @@
 # out_file_1="/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-19951101-reproducibility_test/mem001/mom-o1800-a1800-c1800.out"
 # out_file_2="/g/data/xv83/users/ds0092/active_projects/Squire_2022_CAFE-f6/data/raw/c5-d60-pX-f6-19951101/c5-d60-pX-f6-19951101-mem001/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-19951101/mem001/mom-1800.out"
 
-out_file_1="/g/data/xv83/users/ds0092/active_projects/Squire_2022_CAFE-f6/data/raw/c5-d60-pX-f6-20080501/c5-d60-pX-f6-20080501-mem001/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-20080501/mem001/mom-1800.out"
-out_file_2="/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-20080501-reproducibility_test/mem001/mom-o1800-a1800-c1800.out"
+# out_file_1="/g/data/xv83/users/ds0092/active_projects/Squire_2022_CAFE-f6/data/raw/c5-d60-pX-f6-20080501/c5-d60-pX-f6-20080501-mem001/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-20080501/mem001/mom-1800.out"
+# out_file_2="/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-20080501-reproducibility_test/mem001/mom-o1800-a1800-c1800.out"
+
+out_file_1="/g/data/xv83/users/ds0092/active_projects/Squire_2022_CAFE-f6/data/raw/c5-d60-pX-f6-20090501/c5-d60-pX-f6-20090501-mem001/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-20090501/mem001/mom-1200.out"
+out_file_2="/scratch/ux06/ds0092/CAFE/forecasts/f6/WIP/c5-d60-pX-f6-20090501-reproducibility_test/mem001/mom-o1200-a1200-c1200.out"
 
 start=" ==================Summary of completed MOM integration======================="
 stop="  ===================================================="
