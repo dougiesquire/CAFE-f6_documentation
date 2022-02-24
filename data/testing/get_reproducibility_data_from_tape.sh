5bf6dfd494f16d4961db096826ddf9aecdbe5f23
dougiesquire <dougiesquire@gmail.com>
2022-02-24 14:34:09 +1100

    add section on CAFE60 bias correction change

--- data/testing/get_reproducibility_data_from_tape.sh
+++ data/testing/get_reproducibility_data_from_tape.sh
@@ -8,17 +8,16 @@
 #PBS -l wd
 #PBS -j oe
 
-years=( 2020 ) #( $(seq 1981 1 2020))
-months=( 11 ) #( 05 11 )
+years=( $(seq 1981 1 2020))
+months=( 05 11 )
 
 for year in ${years[@]}; do
 	for month in ${months[@]}; do
 		forecast="c5-d60-pX-f6-${year}${month}01-reproducibility_test"
 		
-		mkdir -p $forecast
-
 		mdss -P xv83 dmls -l f6/${forecast}/${forecast}-mem001.tar > /dev/null 2>&1
 		if [ $? -eq 0 ]; then
+			mkdir -p $forecast		
 			mdss -P xv83 get f6/${forecast}/${forecast}-mem001.tar ./$forecast/
 		
 			mkdir -p ${forecast}/${forecast}-mem001
@@ -27,7 +26,7 @@ for year in ${years[@]}; do
 			# Delete everything except for fms_CM2M.x
 			find ${forecast}/${forecast}-mem001 ! -name 'ocean_scalar_*.nc' -type f -exec rm -f {} +
 			find ${forecast}/${forecast}-mem001 -empty -type d -delete
-			rm -rf ${forecast}/${file}
+			rm -rf ${forecast}/${forecast}-mem001.tar
 		fi
 	done
 done
