91b1a46890a310251bc116c28217e95b2e6f8544
dougiesquire <dougiesquire@gmail.com>
2022-03-21 15:02:34 +1100

    initial commit

---
+++ shell/copy_AGCD_from_CSIRO.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This has to be run from the CSIRO side
+
+variables="precip.nc tmax.nc tmin.nc"
+
+PROJECT_DIR=/g/data/xv83/users/ds0092/active_projects/Squire_2022_CAFE-f6
+
+for variable in variables; do
+	rsync -avPS /datasets/work/af-cdp/work/agcd/climate/${variable} ds0092@gadi-dm.nci.org.au:${PROJECT_DIR}/data/raw/AGCD/climate
+	rsync -avPS /datasets/work/af-cdp/work/agcd/HISTORICAL/climate/${variable} ds0092@gadi-dm.nci.org.au:${PROJECT_DIR}/data/raw/AGCD/HISTORICAL/climate
+done
